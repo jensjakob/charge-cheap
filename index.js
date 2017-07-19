6663f74dcfe5ceb4392e6a73640c6db088788daa
jensjakob <jensjakob@users.noreply.github.com>
2017-07-20 01:52:07 +0200

    Rewrote prices functions and more logs

--- index.js
+++ index.js
@@ -9,6 +9,168 @@ require('dotenv').config()
 
 let q, sessionid, vin
 
+const today = moment().format("YYYY-MM-DD")
+const tomorrow = moment().add(1,"d").format("YYYY-MM-DD")
+
+getPrices("latest", (allPrices) => {
+
+    console.log("Check if you've got tomorrows prices")
+    if (moment(allPrices[0].date).format("YYYY-MM-DD") == tomorrow) {
+        
+        console.log("Todays prices are missing")
+        getPrices(today, (todayPrices) => {
+            allPrices.concat(todayPrices)
+            console.log("Adding todays prices")
+        })
+    }
+
+    console.log("All received prices:")
+    console.log(allPrices)
+
+    allPrices = removeUntilNow(allPrices)
+    console.log("After removal of old prices:")
+    console.log(allPrices)
+
+    allPrices = removeAfter("0700", allPrices)
+    console.log("All prices until 7 am:")
+    console.log(allPrices)
+
+    if (allPrices.length == 0) {
+        console.log("No prices yet, check again in the afternoon")
+    }
+
+	console.log("Finding out how many hours of chargeing is needed")
+	hoursNeeded = hoursNeeded()
+
+    if (hoursNeeded > allPrices.length) {
+        console.log("Not enough hours for a full charge tonight")
+        chargeNow()
+    } else {
+        if (timeIsNow(allPrices, hoursNeeded)) {
+            console.log("Now is a good time to start chargeing")
+            chargeNow()
+        }        
+    }
+})
+
+
+function getPrices(to, _callback) {
+    let arr = new Array()
+
+    if (to == "latest") {
+        options = {
+            area: 'SE3',
+            currency: 'EUR'
+        }
+        console.log("Get latest prices")
+    } else {
+        options = {
+            area: 'SE3',
+            currency: 'EUR',
+            to: to
+        }
+        console.log("Get prices for " + to )
+    }
+
+    prices.hourly(options, (error, results) => {
+		if (error) console.err(error)
+
+		for (let i=0; i<results.length; i++) {
+			arr.push({
+                date: results[i].date.format("YYYY-MM-DD HH:mm"),
+				price: results[i].value.toFixed(2)
+			})
+        }
+
+        _callback(arr)
+    })
+}
+
+function removeUntilNow(arr) {
+    const now = moment().format("HH00")
+
+    for (let key in arr) {
+
+        time = moment(arr[key].date).format("HHmm")
+        
+        if (time < now) {
+            delete arr[key]
+        }
+    }
+
+    return cleanArray(arr)
+}
+
+function removeAfter(hour, arr) {
+
+    for (let key in arr) {
+
+        time = moment(arr[key].date).format("HHmm")
+        
+        if (time > hour) {
+            delete arr[key]
+        }
+    }
+
+    return cleanArray(arr)
+}
+
+function cleanArray(arr) {
+    console.log("Cleaning all deleted prices")
+    return arr.filter(function(n){ return n != undefined });
+}
+
+function hoursNeeded() {
+
+	api("UserLoginRequest", (json) => {
+		if (json.VehicleInfoList) {
+			sessionid = encodeURIComponent(json.VehicleInfoList.vehicleInfo[0].custom_sessionid)
+			vin = encodeURIComponent(json.VehicleInfoList.vehicleInfo[0].vin)
+		} else  {
+			sessionid = encodeURIComponent(json.vehicleInfo[0].custom_sessionid)
+			vin = encodeURIComponent(json.vehicleInfo[0].vin)
+		}
+		api("BatteryStatusCheckRequest", () => {
+			console.log("Receiving data from car... (wait one minute)")
+			setTimeout(() => {
+				api("BatteryStatusRecordsRequest", (json) => {
+					console.log(json)
+					// console.log("Hours needed: " + json.BatteryStatusRecords.TimeRequiredToFull200.HourRequiredToFull)
+
+					if(json.BatteryStatusRecords.PluginState == "NOT_CONNECTED") {
+						console.log("Car not connected")
+					} else {
+						
+						if(json.BatteryStatusRecords.BatteryStatus.BatteryChargingStatus == "NOT_CHARGING") {
+							
+							let hoursNeeded = json.BatteryStatusRecords.TimeRequiredToFull200.HourRequiredToFull
+
+							// If there is need to charge, but not for a full hours
+							if (hoursNeeded == 0) {
+								hoursNeeded = 1
+							}
+
+							// Check to se if we need to add an extra hour, for shorter periods it's better to wait
+							// Don't know if this can be the case, or if it always shows half hours
+							if (json.BatteryStatusRecords.TimeRequiredToFull200.MinutesRequiredToFull > 40) {
+								hoursNeeded += 1
+							}
+
+							console.log("Hours needed: " + hoursNeeded)
+							return hoursNeeded
+
+						} else {
+							console.log("Car is already chargeing, can't stop it")
+						}
+
+					}
+				})
+			}, 60*1000) // wait 60 seconds to get info from car (40 s needed when last tested)
+		})
+		
+	})
+}
+
 function api(action, _callback) {
 	q = ""
 	const region_code = "NE"
@@ -31,12 +193,7 @@ function api(action, _callback) {
 
 	} else {
 
-		// If session does not exist, then login first
-		// if (!sessionid) { // TODO: Best practice?
-		// 	api("UserLoginRequest") // TODO: WAIT!
-		// } else {
 			q = "custom_sessionid=" + sessionid + "&RegionCode=" + region_code + "&VIN=" + vin
-		// }
 
 	} // if login
 
@@ -75,167 +232,31 @@ function api(action, _callback) {
 
 } // function api()
 
-api("UserLoginRequest", (json) => {
-	if (json.VehicleInfoList) {
-		sessionid = encodeURIComponent(json.VehicleInfoList.vehicleInfo[0].custom_sessionid)
-		vin = encodeURIComponent(json.VehicleInfoList.vehicleInfo[0].vin)
-	} else  {
-		sessionid = encodeURIComponent(json.vehicleInfo[0].custom_sessionid)
-		vin = encodeURIComponent(json.vehicleInfo[0].vin)
-	}
-	api("BatteryStatusCheckRequest", () => {
-		console.log("Receiving data from car... (wait one minute)")
-		setTimeout(() => {
-			api("BatteryStatusRecordsRequest", (json) => {
-				console.log(json)
-				// console.log("Hours needed: " + json.BatteryStatusRecords.TimeRequiredToFull200.HourRequiredToFull)
 
-				if(json.BatteryStatusRecords.PluginState == "NOT_CONNECTED") {
-					console.log("Car not connected")
-				} else {
-					
-					if(json.BatteryStatusRecords.BatteryStatus.BatteryChargingStatus == "NOT_CHARGING") {
-						
-						let hoursNeeded = json.BatteryStatusRecords.TimeRequiredToFull200.HourRequiredToFull
+function timeIsNow(allPrices, hoursNeeded) {
 
-						// If there is need to charge, but not for a full hours
-						if (hoursNeeded == 0) {
-							hoursNeeded = 1
-						}
+    allPrices.sort((a, b) => {
+        return a.price-b.price
+    })
+    console.log("Best price first:")
+    console.log(allPrices)
+    
+    const now = moment().format("HH00");
 
-						// Check to se if we need to add an extra hour, for shorter periods it's better to wait
-						// Don't know if this can be the case, or if it always shows half hours
-						if (json.BatteryStatusRecords.TimeRequiredToFull200.MinutesRequiredToFull > 40) {
-							hoursNeeded += 1
-						}
+    for (let i = 0; i < hoursNeeded; i++) {
+        time = moment(allPrices[i].date).format("HHmm");
 
-						console.log("Hours needed: " + hoursNeeded)
+        console.log("Charge at " + time + ", now is " + now)
+        if (time == now) {
+            
+            return true
+            break
 
-						maybeCharge(hoursNeeded);
+        }
+    }
+}
 
-					} else {
-						console.log("Car is already chargeing, can't stop it")
-					}
-
-				}
-			})
-		}, 60*1000) // wait 60 seconds to get info from car (40 s needed when last tested)
-	})
-	
-})
-
-function maybeCharge(hoursNeeded) {
-
-	let pricePerTime = new Array()
-
-	let opts = {
-		area: 'SE3',
-		currency: 'EUR',
-	}
-
-	prices.hourly(opts, function (error, results) {
-		if (error) console.err(error)
-
-		for (let i=0; i<results.length; i++) {
-			let date = results[i].date // moment object (see http://momentjs.com/)
-			let price = results[i].value // float, EUR/MWh
-		
-			pricePerTime.push({
-				date: date.format("YYYY-MM-DD HH:mm"),
-				price: price.toFixed(2)
-			})
-
-			var today = moment().format("YYYY-MM-DD")
-
-			opts = {
-				area: 'SE3',
-				currency: 'EUR',
-				from: today,
-			}
-		}
-
-		prices.hourly(opts, function (error, results) {
-		if (error) console.err(error)
-
-			for (let i=0; i<results.length; i++) {
-				let date = results[i].date // moment object (see http://momentjs.com/)
-				let price = results[i].value // float, EUR/MWh
-			
-				pricePerTime.push({
-					date: date.format("YYYY-MM-DD HH:mm"),
-					price: price.toFixed(2)
-				})
-
-			}
-
-			// Only show future 
-			const now = new Date(moment().minutes(0).seconds(0).milliseconds(0)); //new Date()
-
-			// Tomorrow at 7
-			var end  = new Date()
-			end.setDate(end.getDate() + 1)
-			end.setHours(7) // equals to leaving the car at 8am, but it might need 40 minutes more
-			end.setMinutes(0)
-			end.setMilliseconds(0)
-
-			let time; 
-
-			for (let key in pricePerTime) {
-				time = new Date(pricePerTime[key].date);
-				
-				// Remove old data
-				if (time < now) {
-					delete pricePerTime[key]
-				//	pricePerTime.splice(key,1)
-				}
-
-				// Exclude hours after when it needs to be done (7pm)
-				if (time > end) {
-					delete pricePerTime[key]
-				//	pricePerTime.splice(key,1)
-				}
-			}
-
-			pricePerTime.sort(function(a, b){
-				return a.price-b.price
-			})
-
-			pricePerTime = pricePerTime.filter(function(n){ return n != undefined });
-
-			console.log(pricePerTime)
-			console.log(pricePerTime.length)
-
-			if (hoursNeeded > pricePerTime.length) {
-				console.log("Not enough hours tonight!")
-				hoursNeeded = pricePerTime.length // TODO: Start chargeing without checking hours
-			}
-
-			let theHours = new Array()
-
-			for (let i = 0; i < hoursNeeded; i++) {
-				time = new Date(pricePerTime[i].date);
-
-				if (time.getTime() == now.getTime()) {
-					//charge car if connected and not charging
-					console.log("CHARGE CAR NOW!")
-
-					api("BatteryRemoteChargingRequest", function() {})
-
-				}
-
-				theHours.push({
-					date: pricePerTime[i].date
-				})
-			}
-
-			theHours.sort(function(a, b){
-				var dateA=new Date(a.date), dateB=new Date(b.date)
-    			return dateA-dateB //sort by date ascending
-			})
-			console.log(theHours)
-
-		})
-
-	})
-
+function chargeNow() {
+    console.log("Charge car now")
+    // api("BatteryRemoteChargingRequest", () => {})
 }
